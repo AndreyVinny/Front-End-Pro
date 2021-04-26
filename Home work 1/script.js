85c52d4b0221071fe2419d9932f642dd6bf5567f
AndreyVinny <Noonverto007@gmail.com>
2021-04-26 18:15:16 +0300

    home work 1

--- Home work 1/script.js
+++ Home work 1/script.js
@@ -5,6 +5,6 @@ const h = 30;
 document.write('<p>**************</p>');
 document.write('<p>Обьем цилиндра с площадью основы S = ' + s + ', радиусом R = ' + r + ' и высотой H = ' + h + ' равен: </p>');
 document.write('<p>---------------------</p>');
-document.write('<p>V = ' + Math.PI * h * r * r + '</p>');
+document.write('<p>V = ' + Math.PI * h * Math.pow(r, 2) + '</p>');
 document.write('<p>--------------------</p>');
 document.write('<p>end.</p>');
