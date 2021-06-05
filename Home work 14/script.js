7e3c7fa4d7188dd0e5675d55e91d849325109e22
AndreyVinny <Noonverto007@gmail.com>
2021-06-05 14:30:25 +0300

    home work 14

--- Home work 14/script.js
+++ Home work 14/script.js
@@ -1,12 +1,18 @@
 let list = [1,2,3,4,5];
-let newList = copy(list, function(value){return value * 10;});
 
 function copy(list, func){
     let a = [];
     for(let i = 0; i < list.length; i++)
-        a[i] = func(list[i]);
-    return a;
+        if (typeof func == 'function'){
+            a[i] = func(list[i]);
+        } else
+            a[i] = list[i];
+        
+        return a;
 }
 
-console.log(list);
-console.log(newList);
+let withFunc = copy(list, function(value){return value * 10;});
+let withoutFunc = copy(list, 155);
+
+console.log(withFunc);
+console.log(withoutFunc);
