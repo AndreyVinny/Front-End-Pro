07fa2d7d81de56912fb8e393f49b0f857d449f52
AndreyVinny <Noonverto007@gmail.com>
2021-06-30 11:34:52 +0300

    home work24

--- Home work 24/script.js
+++ Home work 24/script.js
@@ -4,15 +4,15 @@ function Animal (name, icon, tipe, breed) {
     this.tipe = tipe;
     this.breed = breed;
     
-    function getVoice(voice) {
-        if(voice === `dog`)
-            return `I can say gav-gav.`
-        else if(voice === `cat`)
-            return `I can say myu-myu.`
-        else 
-            return ``; 
-    }
-        return console.log(`Hello, my name is ${name} ${icon}. I'm ${breed} ${tipe}. ${getVoice(tipe)}`);
+        function getVoice(voice) {
+            if(voice === `dog`)
+                return `I can say gav-gav.`
+            else if(voice === `cat`)
+                return `I can say myu-myu.`
+            else 
+                return ``; 
+        }
+            return console.log(`Hello, my name is ${name} ${icon}. I'm ${breed} ${tipe}. ${getVoice(tipe)}`);
 }
 
 let Ivan = new Animal('Ivan', '🐕', 'dog', 'Australian Shepherds'),
